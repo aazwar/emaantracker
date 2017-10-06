f486687e6c9bb06fb7f44e1f349cf1152527d9d5
aazwar <azrul.azwar@gmail.com>
2017-10-06 20:49:12 +0700

    Searching for expo components' alternative

---
+++ app/Icons.js
@@ -0,0 +1,13 @@
+import { default as Entypo } from 'react-native-vector-icons/Entypo';
+import { default as EvilIcons } from 'react-native-vector-icons/EvilIcons';
+import { default as Feather } from 'react-native-vector-icons/Feather';
+import { default as FontAwesome } from 'react-native-vector-icons/FontAwesome';
+import { default as Foundation } from 'react-native-vector-icons/Foundation';
+import { default as Ionicons } from 'react-native-vector-icons/Ionicons';
+import { default as MaterialIcons } from 'react-native-vector-icons/MaterialIcons';
+import { default as MaterialCommunityIcons } from 'react-native-vector-icons/MaterialCommunityIcons';
+import { default as Octicons } from 'react-native-vector-icons/Octicons';
+import { default as SimpleLineIcons } from 'react-native-vector-icons/SimpleLineIcons';
+import { default as Zocial } from 'react-native-vector-icons/Zocial';
+
+export { Entypo, EvilIcons, Foundation, FontAwesome, Ionicons, MaterialIcons, MaterialCommunityIcons, Octicons, SimpleLineIcons, Zocial };
